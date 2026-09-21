efbbc3ce49c74823c4f4920ccc7e6c819d8a65fa
Jeevan Bikash Sahoo <sahoojeevanbikash@gmail.com>
2026-09-21 15:45:46 +0530

    Add files via upload

---
+++ Patterns/pattern 1.java
@@ -0,0 +1,47 @@
+/* 896. Pattern 1
+Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+
+*****
+
+*****
+
+*****
+
+*****
+
+*****
+
+Print the pattern in the function given to you.
+
+Example 1:
+Input: n = 4
+
+Output:
+****
+****
+****
+****
+
+Example 2:
+Input: n = 2
+
+Output:
+**
+**
+
+Constraints:
+1 <= n <= 100
+*/ 
+
+
+
+class Solution {
+  public void pattern1(int n) {
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        System.out.print("*");
+      }
+      System.out.println("");
+    }
+  }
+}
